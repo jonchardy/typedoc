57ccb8a6f7ea9d6438fe55823652c6b215e8cc42
Jon Hardy <jonathan.c.hardy@gmail.com>
2018-04-06 11:45:37 -0400

    Scrap tests part 2
    A few more updates

--- gruntfile.js
+++ gruntfile.js
@@ -64,34 +64,10 @@ module.exports = function(grunt)
                 }
             }
         },
-        copy:  {
-            staticTestFiles: {
-                expand: true,
-                cwd: 'src',
-                src: [
-                    'test/converter/**/*',
-                    'test/renderer/**/*'
-                ],
-                dest: 'dist/'
-            }
-        },
-        clean: {
-            specsBefore: ['src/test/renderer/specs'],
-            specsAfter: ['src/test/renderer/specs/assets']
-        },
         watch: {
             source: {
                 files: ['src/**/*.ts'],
                 tasks: ['ts:typedoc', 'string-replace:version']
-            }
-        },
-        mocha_istanbul: {
-            coverage: {
-                src: 'dist/test',
-                options: {
-                    mask: '*.js',
-                    timeout: 10000
-                }
             }
         }
     });
@@ -99,79 +75,10 @@ module.exports = function(grunt)
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-string-replace');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-ts');
     grunt.loadNpmTasks('grunt-tslint');
-    grunt.loadNpmTasks('grunt-mocha-istanbul');
 
     grunt.registerTask('default', ['tslint', 'ts:typedoc', 'string-replace:version']);
-    grunt.registerTask('build_and_test', ['default', 'specs', 'copy', 'mocha_istanbul:coverage']);
-    grunt.registerTask('specs', ['clean:specsBefore', 'build-specs', 'clean:specsAfter']);
-
-    grunt.registerTask('build-specs', function() {
-        var FS = require('fs-extra');
-        var Path = require('path');
-        var TypeDoc = require('./');
-
-        var base = Path.join(__dirname, 'src', 'test', 'converter');
-        var app = new TypeDoc.Application({
-            mode:   'Modules',
-            target: 'ES5',
-            module: 'CommonJS',
-            experimentalDecorators: true,
-            jsx: 'react',
-            lib: [
-                "lib.dom.d.ts",
-                "lib.es5.d.ts",
-                "lib.es2015.iterable.d.ts",
-                "lib.es2015.collection.d.ts"
-            ],
-        });
-
-        FS.readdirSync(Path.join(base)).forEach(function(directory) {
-            console.log(directory);
-
-            var path = Path.join(base, directory);
-            if (!FS.lstatSync(path).isDirectory()) return;
-            TypeDoc.resetReflectionID();
-
-            var src = app.expandInputFiles([path]);
-            var out = Path.join(base, directory, 'specs.json');
-            var result = app.convert(src);
-            var data = JSON.stringify(result.toObject(), null, '  ');
-            data = data.split(TypeDoc.normalizePath(base)).join('%BASE%');
-            FS.writeFileSync(out, data);
-        });
-
-        var src = Path.join(__dirname, 'examples', 'basic', 'src');
-        var out = Path.join(__dirname, 'src', 'test', 'renderer', 'specs');
-
-        FS.removeSync(out);
-        app.generateDocs(app.expandInputFiles([src]), out);
-
-
-        function getFileIndex(base, dir, results) {
-            results = results || [];
-            dir = dir || '';
-            var files = FS.readdirSync(Path.join(base, dir));
-            files.forEach(function(file) {
-                file = Path.join(dir, file);
-                if (FS.statSync(Path.join(base, file)).isDirectory()) {
-                    getFileIndex(base, file, results);
-                } else {
-                    results.push(file);
-                }
-            });
-
-            return results.sort();
-        }
-
-        FS.removeSync(Path.join(out, 'assets'));
-        var gitHubRegExp = /https:\/\/github.com\/[A-Za-z0-9\-]+\/typedoc\/blob\/[^\/]*\/examples/g;
-        getFileIndex(out).forEach(function (file) {
-            file = Path.join(out, file);
-            FS.writeFileSync(file, FS.readFileSync(file, {encoding:'utf-8'}).replace(gitHubRegExp, 'https://github.com/sebastian-lenz/typedoc/blob/master/examples'));
-        });
-    });
+    // grunt.registerTask('build_and_test', ['default'/*, 'specs', 'copy', 'mocha_istanbul:coverage'*/]);
+    // grunt.registerTask('specs', ['clean:specsBefore', 'build-specs', 'clean:specsAfter']);
 };
